2435395f1d25efda7efb527d1515bbe02dd64402
Carlos Peña <carlos.pe.aja@gmail.com>
2022-07-29 20:41:26 -0500

    Clase 6: creacion de seccion de categorias(generos)

--- src/main.js
+++ src/main.js
@@ -21,4 +21,29 @@ async function getTrendingMoviesPreview() {
     });
 }
 
+async function getGenresPreview() {
+    const res = await fetch(API_URL + '/genre/movie/list?api_key=' + API_KEY);
+    const data = await res.json();
+    const genres = data.genres;
+
+    console.log(data);
+    console.log(genres);
+
+    genres.forEach(genre => {
+        const genrePreview = document.querySelector('#categoriesPreview .categoriesPreview-list');
+
+        const genreContainer = document.createElement('div');
+        genreContainer.classList.add('category-container');
+
+        const genreName = document.createElement('h3');
+        genreName.classList.add('category-title');
+        genreName.setAttribute('id', ('id' + genre.id));
+        genreName.innerHTML = genre.name;
+
+        genreContainer.appendChild(genreName);
+        genrePreview.appendChild(genreContainer);
+    });
+}
+
 getTrendingMoviesPreview();
+getGenresPreview();
